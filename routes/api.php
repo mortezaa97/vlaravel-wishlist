370dab696cc01b3c1835d9bc12a0dd9c23e78bcd
mortezaa <mortezajafari76@gmail.com>
2025-09-25 18:55:39 +0330

    fix:issues

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Mortezaa97\Wishlist\Http\Controllers\WishlistController;
 
 Route::prefix('api')->group(function () {
+    Route::get('wishlists', [WishlistController::class, 'index'])->name('wishlists.index');
+    Route::get('wishlists/{wishlist}', [WishlistController::class, 'show'])->name('wishlists.show');
     Route::post('wishlists', [WishlistController::class, 'store'])->name('wishlists.store');
 });
