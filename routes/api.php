abae373370506a09a3ef13aca38c67698408efc5
mortezaa <mortezajafari76@gmail.com>
2025-09-27 13:13:38 +0330

    fix:route api middleware issue

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Mortezaa97\Wishlist\Http\Controllers\WishlistController;
 
-Route::prefix('api')->group(function () {
+Route::prefix('api')->middleware('api')->group(function () {
     Route::get('wishlists', [WishlistController::class, 'index'])->name('wishlists.index');
     Route::get('wishlists/{wishlist}', [WishlistController::class, 'show'])->name('wishlists.show');
     Route::post('wishlists', [WishlistController::class, 'store'])->name('wishlists.store');
